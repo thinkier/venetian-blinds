322c417dbb6c0cb1da40263edb90ff74e838a80e
thinkier <26728734+thinkier@users.noreply.github.com>
2023-06-23 00:00:25 +1000

    testing the rp2040 uart connection to the pi 4B

--- firmware/SerialTesting/SerialTesting.ino
+++ firmware/SerialTesting/SerialTesting.ino
@@ -1,16 +1,14 @@
 #include <Arduino.h>
 
-const long SERIAL_BAUD_RATE = 115200;
+const long SERIAL_BAUD_RATE = 2000000;
 
 void setup() {
     Serial.begin(SERIAL_BAUD_RATE);
     Serial1.begin(SERIAL_BAUD_RATE);
-    Serial2.begin(SERIAL_BAUD_RATE);
 }
 
 void loop() {
     Serial.println("This is RP2040 on BTT SKR Pico v1.0 via SerialUSB");
     Serial1.println("This is RP2040 on BTT SKR Pico v1.0 via Serial1");
-    Serial2.println("This is RP2040 on BTT SKR Pico v1.0 via Serial2");
     delay(1000);
 }
